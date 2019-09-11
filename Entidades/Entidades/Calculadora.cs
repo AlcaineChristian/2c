8bbde3da81b1add32ee396688d5805a158191750
Alcaine Tomas <40808690+AlcaineChristian@users.noreply.github.com>
2019-09-11 19:03:22 -0300

    Add files via upload

---
+++ Entidades/Entidades/Calculadora.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Calculadora
+    {
+        public double Operar(Numero num1,Numero num2, string operador)
+        {
+            double ret=0;
+
+
+            return ret;
+        }
+        public string ValidarOperador(string operador)
+        {
+            string ret = "";
+
+
+            return ret;
+        }
+
+
+    }
+}
---
+++ Entidades/Entidades/Numero.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        private double numero;
+
+        public Numero()
+        {
+            this.numero = 0;
+        }
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+        public Numero(string strNumero)
+        {
+            double.TryParse(strNumero, out this.numero);
+        }
+
+
+
+        private static double ValidarNumero(string strNumero)
+        {
+            double ret = 0;
+
+            return ret;
+        }
+
+        public string BinarioDecimal(string binario)
+        {
+            string ret = "";
+            return ret;
+        }
+
+        public string DecimalBinario(double numero)
+        {
+            string ret = "";
+            return ret;
+        }
+        public string DecimalBinario(string strNumero)
+        {
+            string ret = "";
+            return ret;
+        }
+
+
+
+
+    }
+}
